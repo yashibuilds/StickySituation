47e24bf4af43c10ac3a2ff4c4b5b9d5b2d90e9e0
yashi <yashibuilds@gmail.com>
2026-03-01 18:03:53 -0800

    Add PlayerMovement script. Player can now navigate around desks

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public float moveSpeed = 6.7f;
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2(h, v).normalized * moveSpeed;
     }
 }
