ccefb0d0930bef84c3988775a248d89beb5d7ec2
yashi <yashibuilds@gmail.com>
2026-03-01 19:44:28 -0800

    Implement Nerd behavior. Not done because I still have to implement GameManager

---
+++ Assets/Scripts/NerdEnemy.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class NerdEnemy : MonoBehaviour
+{
+    public bool isSilenced = false; // tells you if the nerd has gum on his face or not
+    public float detectionRange = 6.7f; // tells you how close the player gets before the nerd starts tattling.
+    private GameObject player; // storing the player so we can access position info about them
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSilenced) { // first check if nerd is silenced. if so, nothing happens.
+            return;
+        }
+
+        float playerNerdDistance = Vector2.Distance(transform.position, player.transform.position);
+        if (playerNerdDistance < detectionRange) {
+            Debug.Log("Nerd sees you!")
+        }
+    }
+
+    public void GetSilenced() {
+        isSilenced = true;
+        // use a gum (life). haven't set up GameManager so i'll wait to do this
+    }
+
+    // if gum collides with nerd, we need to trigger GetSilenced
+    // write function later 
+}
